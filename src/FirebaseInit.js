7bd922116664b74f87d7a6e6c7c74fe9d4abdf05
pratik <lazy.monk.13@gmail.com>
2016-08-26 11:07:43 -0700

    first commit

---
+++ src/FirebaseInit.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyAvNDrIlq8y0vHOOGZBPiKE3BRxwVKVxaA",
+  authDomain: "testapp-88060.firebaseapp.com",
+  databaseURL: "https://testapp-88060.firebaseio.com",
+  storageBucket: "testapp-88060.appspot.com",
+};
+
+firebase.initializeApp(config);
